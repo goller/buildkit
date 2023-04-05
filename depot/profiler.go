884ce6e33d12e131220b9f2205f46a4beff49475
Chris Goller <goller@gmail.com>
2023-04-05 13:14:57 -0500

    feat: add optional profiling when env vars are set
    We have seen performance issues seemingly related to
    the session not closing.  We want to use profiling to
    capture some of the root causes.
    
    Because profiling can be expensive, we only turn it on
    when the env vars, PROFILER_TOKEN, PROFILER_ENDPOINT,
    and PROFILER_PROJECT_ID are set.
    
    Signed-off-by: Chris Goller <goller@gmail.com>

---
+++ depot/profiler.go
@@ -0,0 +1,60 @@
+package depot
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/moby/buildkit/version"
+	"github.com/pyroscope-io/client/pyroscope"
+)
+
+type ProfilerOpts struct {
+	Endpoint  string
+	Token     string
+	ProjectID string
+	Version   string
+}
+
+// NewProfilerOptsFromEnv pulls settings from env.  If required settings
+// are not in the env, it will return nil.
+func NewProfilerOptsFromEnv() *ProfilerOpts {
+	endpoint := os.Getenv("PROFILER_ENDPOINT")
+	token := os.Getenv("PROFILER_TOKEN")
+	projectID := os.Getenv("PROFILER_PROJECT_ID")
+	version := version.Version
+
+	if endpoint == "" || token == "" || projectID == "" {
+		return nil
+	}
+
+	return &ProfilerOpts{
+		Endpoint:  endpoint,
+		Token:     token,
+		ProjectID: projectID,
+		Version:   version,
+	}
+}
+
+// Starts exporting profiles to pyroscope if opts is not nil.
+func StartProfiler(opts *ProfilerOpts) {
+	if opts != nil {
+		runtime.SetMutexProfileFraction(5)
+		runtime.SetBlockProfileRate(10000)
+		_, _ = pyroscope.Start(pyroscope.Config{
+			ApplicationName: "depot-buildkitd",
+			ServerAddress:   opts.Endpoint,
+			Logger:          nil,
+			Tags:            map[string]string{"version": opts.Version, "projectID": opts.ProjectID},
+			AuthToken:       opts.Token,
+
+			ProfileTypes: []pyroscope.ProfileType{
+				pyroscope.ProfileCPU,
+				pyroscope.ProfileGoroutines,
+				pyroscope.ProfileMutexCount,
+				pyroscope.ProfileMutexDuration,
+				pyroscope.ProfileBlockCount,
+				pyroscope.ProfileBlockDuration,
+			},
+		})
+	}
+}
