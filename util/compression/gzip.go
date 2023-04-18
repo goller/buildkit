47c1b094e1360b3bb538a79602c38b2a4ac570fa
Chris Goller <goller@gmail.com>
2023-04-18 14:18:25 -0500

    feat: optionally turn off parallel gzip
    Signed-off-by: Chris Goller <goller@gmail.com>

--- util/compression/gzip.go
+++ util/compression/gzip.go
@@ -1,11 +1,13 @@
 package compression
 
 import (
+	"compress/gzip"
 	"context"
 	"io"
+	"os"
 
 	// DEPOT: Using parallel gzip for faster image layer compression
-	gzip "github.com/klauspost/pgzip"
+	"github.com/klauspost/pgzip"
 
 	"github.com/containerd/containerd/content"
 	"github.com/containerd/containerd/images"
@@ -62,10 +64,21 @@ func (c gzipType) String() string {
 
 func gzipWriter(comp Config) func(io.Writer) (io.WriteCloser, error) {
 	return func(dest io.Writer) (io.WriteCloser, error) {
-		level := gzip.DefaultCompression
+		// DEPOT: Using parallel gzip for faster image layer compression.
+		// We have the logic inverted here so that we can continue to use the integration tests.
+		useStdGzip := os.Getenv("DEPOT_DISABLE_PARALLEL_GZIP") != ""
+		if useStdGzip {
+			level := gzip.DefaultCompression
+			if comp.Level != nil {
+				level = *comp.Level
+			}
+			return gzip.NewWriterLevel(dest, level)
+		}
+
+		level := pgzip.DefaultCompression
 		if comp.Level != nil {
 			level = *comp.Level
 		}
-		return gzip.NewWriterLevel(dest, level)
+		return pgzip.NewWriterLevel(dest, level)
 	}
 }
